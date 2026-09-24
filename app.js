3574b7f4eb2f4a5349e14a99bdab958aacce609f
pietromarchioro2001 <pietromarchioro2001@gmail.com>
2026-09-24 09:39:35 +0200

    Commit 1 - Home UI

---
+++ app.js
@@ -0,0 +1,8 @@
+
+document.getElementById("checkin").onclick = () => {
+    alert("Qui si aprirà la fotocamera OCR");
+};
+
+document.getElementById("checkout").onclick = () => {
+    alert("Qui comparirà la lista dei veicoli aperti");
+};
